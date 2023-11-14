707a05286d02a665eaefe4010e70294d9d40600f
felipeleonn <felipeleon98@outlook.com>
2023-11-14 18:17:59 -0300

    finished setting create-prompt page, prompt's model and debugged user's creat model to mongoDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next"
 import GoogleProvider from 'next-auth/providers/google'
+
 import User from '@models/user'
 import { connectToDB } from '@utils/database'
 
@@ -10,43 +11,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
 
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    })
-    session.user.id = sessionUser._id.toString()
-    return session
-  },
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
 
-  async signIn({profile}) {
-    try {
-      await connectToDB()
-
-      // check if user exists
-      const userExists  = await User.findOne({
-        email: profile.email
-      })
-
-      
-      // if not, create new user and save it to the DB
-      if(!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture
-        })
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
       }
-      return true
-      
-    } catch (error) {
-      console.log(error)
-      return false
-    }
+    },
   }
 })
-
 
 export { handler as GET, handler as POST }
