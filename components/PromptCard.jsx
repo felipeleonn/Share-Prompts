4fd54b6eb5563265048279257e0a5024b5ae8090
felipeleonn <felipeleon98@outlook.com>
2023-11-21 18:03:02 -0300

    create edit and delete in myProfile finished and debuged - note: findByIdAndRemove() doesn't work in mongoose, instead use findByInAndDelete()

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,12 +8,16 @@ import { usePathname, useRouter } from 'next/navigation'
 
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
-  const [copied, setcopied] = useState('')
+  const {data: session} = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState('')
 
   const handleCopy = () => {
-    setcopied(post.prompt)
+    setCopied(post.prompt)
     navigator.clipboard.writeText(post.prompt)
-    setTimeout(() => setcopied(""), 3000)
+    setTimeout(() => setCopied(""), 3000)
   }
   return (
     <div className='prompt_card'>
@@ -49,8 +53,23 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
 
       <p className='font-inter text-sm blue_gradient cursor-pointer' 
       onClick={() => handleTagClick && handleTagClick(post.tag)}>
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p className='font-inter text-sm green_gradient cursor-pointer'
+          onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p className='font-inter text-sm orange_gradient cursor-pointer'
+          onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
